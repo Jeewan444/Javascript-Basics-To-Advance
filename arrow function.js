0fff26dd043d40109f4d4da3a9b41dc2bf959078
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2024-08-05 21:36:05 +0530

    Update arrow function.js

--- arrow function.js
+++ arrow function.js
@@ -18,3 +18,20 @@ const arowstring = () => {
     console.log("hello world");
 }
 arowstring();
+
+// How can you write an arrow function in JavaScript
+//that takes an array of strings and returns an object where the keys are the strings and the values are the lengths of those strings?
+
+const getStringLengths = (strings) => {
+  return strings.reduce((acc, str) => {
+    acc[str] = str.length;
+    return acc;
+  }, {});
+};
+
+
+const stringsArray = ["apple", "banana", "cherry"];
+const lengthsObject = getStringLengths(stringsArray);
+
+console.log(lengthsObject); 
+
