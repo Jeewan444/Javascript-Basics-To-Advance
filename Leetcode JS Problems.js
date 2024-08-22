c49a727c347a25dbfab260779cb458837520aa47
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2024-08-22 21:30:40 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -117,3 +117,18 @@ var strangePrinter = function(s) {
 
     return dp[0][n - 1];
 };
+
+
+/*the complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+
+For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+Given an integer num, return its complement.
+*/
+
+var findComplement = function(num) {
+    let mask = 1;
+    while (mask <= num) {
+        mask <<= 1;
+    }
+    return mask - 1 - num;
+};
