cf2a7f5c3ab9aa415f5838795dad785d505bf126
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2024-09-03 20:36:38 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -294,3 +294,52 @@ var maxProbability = function(n, edges, succProb, start, end) {
 
     return 0;
 };
+
+
+
+/* you are given a string s consisting of lowercase English letters, and an integer k.
+
+First, convert s into an integer by replacing each letter with its position in the alphabet (i.e., replace 'a' with 1, 'b' with 2, ..., 'z' with 26). Then, transform the integer by replacing it with the sum of its digits. Repeat the transform operation k times in total.
+
+For example, if s = "zbax" and k = 2, then the resulting integer would be 8 by the following operations:
+
+
+
+Convert: "zbax" ➝ "(26)(2)(1)(24)" ➝ "262124" ➝ 262124
+Transform #1: 262124 ➝ 2 + 6 + 2 + 1 + 2 + 4 ➝ 17
+Transform #2: 17 ➝ 1 + 7 ➝ 8
+
+
+*/
+
+
+var getLucky = function(s, k) {
+    // Helper function to sum the digits of a number
+    const transform = function(t) {
+        let res = 0;
+        while (t > 0) {
+            res += t % 10;
+            t = Math.floor(t / 10);
+        }
+        return res;
+    };
+    
+    // Convert the string to the sum of its corresponding numeric values
+    let res = 0;
+    for (let ch of s) {
+        let num = ch.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+        if (num >= 10) {
+            res += num % 10 + Math.floor(num / 10);
+        } else {
+            res += num;
+        }
+    }
+
+    // Perform the transformation k-1 times
+    while (--k > 0) {
+        res = transform(res);
+    }
+
+    // Return the final result after k transformations
+    return res;
+};
