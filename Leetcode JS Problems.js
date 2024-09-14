0800662f2b8b11bbcaf20863888ecbf1437c1d05
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2024-09-14 21:01:35 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -697,3 +697,49 @@ var xorQueries = function(arr, queries) {
  -----------------------------------------------------------------------------------------------------------
 
 
+  // 2419 .
+
+//  Longest Sub-array with Maximum Bitwise AND   //
+
+//   You are given an integer array nums of size n.
+
+// Consider a non-empty subarray from nums that has the maximum possible bitwise AND.
+
+// In other words, let k be the maximum value of the bitwise AND of any subarray of nums. Then, only subarrays with a bitwise AND equal to k should be considered.
+// Return the length of the longest such subarray.
+
+// The bitwise AND of an array is the bitwise AND of all the numbers in it.
+
+// A subarray is a contiguous sequence of elements within an array.
+
+
+
+
+// solution
+
+  var longestSubarray = function(nums) {
+    let maxBitwiseAnd = Math.max(...nums);
+    let maxi = 1;
+    let count = 0;
+    let i = 0;
+    
+    while (i < nums.length) {
+        if (nums[i] === maxBitwiseAnd) {
+            while (i < nums.length && nums[i] === maxBitwiseAnd) {
+                count++;
+                i++;
+            }
+            maxi = Math.max(maxi, count);
+            count = 0;
+        } else {
+            i++;
+        }
+    }
+    
+    return maxi;
+};
+
+ -----------------------------------------------------------------------------------------------------------
+ 
+ -----------------------------------------------------------------------------------------------------------
+
