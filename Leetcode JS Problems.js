a058d25b6de5ac646d8f2d2152d95335de3e63ba
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2024-09-16 20:25:19 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -780,3 +780,49 @@ var xorQueries = function(arr, queries) {
  
  -----------------------------------------------------------------------------------------------------------
 
+
+//539 . minimum time difference 
+
+//  Given a list of 24-hour clock time points in "HH:MM" format, return the minimum minutes difference between any two time-points in the list.
+ 
+
+// Example 1:
+
+// Input: timePoints = ["23:59","00:00"]
+// Output: 1
+// Example 2:
+
+// Input: timePoints = ["00:00","23:59","00:00"]
+// Output: 0
+
+
+
+
+
+
+
+ /**
+ * @param {string[]} timePoints
+ * @return {number}
+ */
+var findMinDifference = function(timePoints) {
+    // Convert times to minutes
+    let minutes = timePoints.map(time => {
+        let [h, m] = time.split(':').map(Number);
+        return h * 60 + m;
+    });
+
+    // Sort times in ascending order
+    minutes.sort((a, b) => a - b);
+
+    // Find minimum difference across adjacent elements
+    let minDiff = Infinity;
+    for (let i = 0; i < minutes.length - 1; i++) {
+        minDiff = Math.min(minDiff, minutes[i + 1] - minutes[i]);
+    }
+
+    // Consider the circular difference between last and first element
+    minDiff = Math.min(minDiff, 24 * 60 - minutes[minutes.length - 1] + minutes[0]);
+
+    return minDiff;
+};
