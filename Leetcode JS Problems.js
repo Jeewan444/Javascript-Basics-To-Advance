160395591a616b3dd029db9385bd452f05f1424a
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2024-09-23 18:55:16 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -1148,3 +1148,44 @@ var lexicalOrder = function(n) {
 -----------------------------------------------------------------------------------------------------------
  
 -----------------------------------------------------------------------------------------------------------
+
+//2707. extra characters in a string 
+
+ 
+// You are given a 0-indexed string s and a dictionary of words dictionary. You have to break s into one or more non-overlapping substrings such that each substring is present in dictionary. There may be some extra characters in s which are not present in any of the substrings.
+
+// Return the minimum number of extra characters left over if you break up s optimally.
+
+ 
+
+// Example 1:
+
+// Input: s = "leetscode", dictionary = ["leet","code","leetcode"]
+// Output: 1
+// Explanation: We can break s in two substrings: "leet" from index 0 to 3 and "code" from index 5 to 8. There is only 1 unused character (at index 4), so we return 1.
+
+ 
+ var minExtraChar = function(s, dictionary) {
+    const n = s.length;
+    const dictSet = new Set(dictionary);
+    const dp = Array(n + 1).fill(0);
+
+    for (let i = n - 1; i >= 0; i--) {
+        dp[i] = dp[i + 1] + 1; // Worst case: add one character
+        for (let j = i; j < n; j++) {
+            const substring = s.slice(i, j + 1);
+            if (dictSet.has(substring)) {
+                dp[i] = Math.min(dp[i], dp[j + 1]);
+            }
+        }
+    }
+    return dp[0];
+};
+
+-----------------------------------------------------------------------------------------------------------
+ 
+-----------------------------------------------------------------------------------------------------------
+
+
+
+ 
