4374bb368c5febb3de6c57ae874b21f3ad541683
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2024-09-30 21:28:53 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -1409,7 +1409,48 @@ class MyCalendarTwo {
  * boolean param_1 = obj.book(start,end);
  */
 
+-----------------------------------------------------------------------------------------------------------
+ 
+-----------------------------------------------------------------------------------------------------------
 
+//1381 .
+
+class CustomStack {
+    int [] stack;
+    int top = -1;
+
+    public CustomStack(int maxSize) {
+        this.stack = new int [maxSize];
+    }
+    
+    public void push(int x) {
+        if (top < this.stack.length - 1) {
+            top ++;
+            this.stack[top] = x;
+        }
+    }
+    
+    public int pop() {
+        if (top != -1) {
+            return this.stack[top --];
+        }
+        return -1;
+    }
+    
+    public void increment(int k, int val) {
+        for (int i = 0; i < Math.min(k, top + 1); i ++) {
+            this.stack[i] += val;
+        }
+    }
+}
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack obj = new CustomStack(maxSize);
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * obj.increment(k,val);
+ */
 
 
 -----------------------------------------------------------------------------------------------------------
