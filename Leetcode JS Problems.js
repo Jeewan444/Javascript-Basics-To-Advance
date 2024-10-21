d0158fc0fbd38b3516e90946d83a4edfa8dea16d
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2024-10-21 22:09:58 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -2101,7 +2101,27 @@ class Solution {
  
 -----------------------------------------------------------------------------------------------------------
 
-
+//1593.
+ class Solution {
+    public int maxUniqueSplit(String s) {
+        return backtrack(0, s, new HashSet<>());
+    }
+    private int backtrack(int start, String s, HashSet<String> seen) {
+        if (start == s.length()) {
+            return 0; 
+        }
+        int maxSplits = 0;
+        for (int end = start + 1; end <= s.length(); end++) {
+            String substring = s.substring(start, end);
+            if (!seen.contains(substring)) {
+                seen.add(substring); 
+                maxSplits = Math.max(maxSplits, 1 + backtrack(end, s, seen));
+                seen.remove(substring);
+            }
+        }
+        return maxSplits;
+    }
+}
 
 
 
