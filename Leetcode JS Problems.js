82e7d84cf3a8cbcbe68156c83d0f7ee22bda6f20
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2024-11-06 20:48:14 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -2715,7 +2715,28 @@ class Solution {
  
 -----------------------------------------------------------------------------------------------------------
 
+//3011.
 
+ class Solution {
+    public static boolean canSortArray(final int[] nums) {
+        short pmax = 0, cmin = 0, cmax = 0;
+        byte pcnt = 0;
+        for (final int v : nums) {
+            final byte ccnt = (byte)Integer.bitCount(v);
+            if (pcnt == ccnt) {
+                cmin = cmin > v ? (short)v : cmin;
+                cmax = cmax < v ? (short)v : cmax;
+            } else if (cmin < pmax) {
+                return false;
+            } else {
+                pmax = cmax;
+                cmin = cmax = (short)v;
+                pcnt = ccnt;
+            }
+        }
+        return cmin >= pmax;
+    }
+}
 
 
 
