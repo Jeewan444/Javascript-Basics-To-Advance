2da7e92de03022a92e0c4855371b838381b6dd5e
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2024-11-12 20:49:03 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -1,4 +1,4 @@
-/* problem no 2665
+ /* problem no 2665
 
 Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
 
@@ -2902,7 +2902,44 @@ class Solution {
  
 -----------------------------------------------------------------------------------------------------------
 
+//2070.
 
+ import java.util.*;
+
+public class Solution {
+    public int[] maximumBeauty(int[][] items, int[] queries) {
+        int maxI = Integer.MAX_VALUE;
+        List<int[]> res = new ArrayList<>();
+        res.add(new int[] {0, 0, maxI});
+
+        Arrays.sort(items, Comparator.comparingInt(a -> a[0]));
+
+        for (int[] item : items) {
+            int price = item[0];
+            int beauty = item[1];
+            int[] lastBracket = res.get(res.size() - 1);
+
+            if (beauty > lastBracket[1]) {
+                lastBracket[2] = price;
+                res.add(new int[] {price, beauty, maxI});
+            }
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int j = 0; j < queries.length; j++) {
+            int x = queries[j];
+            for (int i = res.size() - 1; i >= 0; i--) {
+                if (res.get(i)[0] <= x) {
+                    ans[j] = res.get(i)[1];
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
 
 
 
