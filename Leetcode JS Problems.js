86dee1b588b57c14cfaff47f95653b7992ebeb9c
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2024-12-02 23:59:28 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -3671,7 +3671,21 @@ public class Solution {
  
 -----------------------------------------------------------------------------------------------------------
 
+//1455.
+ class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] arr = sentence.split(" ");               
+        int n=arr.length;
+        
+        for(int i=0; i<n; i++){
+            if(arr[i].startsWith(searchWord)){                
+                return i+1;
+            }
+        }
 
+        return -1;
+    }
+}
 
 
 -----------------------------------------------------------------------------------------------------------
