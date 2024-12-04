1c106db878fd10f04ce9cd2f5a91d1a7e8900256
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2024-12-04 20:56:58 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -3715,7 +3715,20 @@ public class Solution {
  
 -----------------------------------------------------------------------------------------------------------
 
+//2825.
 
+  class Solution {
+    public boolean canMakeSubsequence(String source, String target) {
+        int targetIdx = 0, targetLen = target.length();
+        
+        for (char currChar : source.toCharArray()) {
+            if (targetIdx < targetLen && (target.charAt(targetIdx) - currChar + 26) % 26 <= 1) {
+                targetIdx++;
+            }
+        }
+        return targetIdx == targetLen;
+    }
+}
 
 
 
