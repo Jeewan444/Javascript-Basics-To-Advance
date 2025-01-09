beae2cfbbda7acab7f249ca3f02930b778633c8e
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2025-01-09 14:02:22 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -5060,3 +5060,17 @@ class Solution {
 -----------------------------------------------------------------------------------------------------------
  
 -----------------------------------------------------------------------------------------------------------
+//2185.
+
+ class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int c = 0;
+        int n = pref.length();
+        for (String w : words) {
+            if (w.length() >= n && w.startsWith(pref)) {
+                c++;
+            }
+        }
+        return c;
+    }
+}
