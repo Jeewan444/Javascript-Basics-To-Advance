94c7aa31d60456ce02f4239deb2bc965bf999309
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2025-01-13 20:49:13 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -5198,7 +5198,25 @@ class Solution {
 -----------------------------------------------------------------------------------------------------------
  
 -----------------------------------------------------------------------------------------------------------
-
+//3223.
+ class Solution {
+    public int minimumLength(String s) {
+        int[] charFrequency = new int[26];
+        int totalLength = 0;
+        for (char c : s.toCharArray()) {
+            charFrequency[c - 'a']++;
+        }
+        for (int frequency : charFrequency) {
+            if (frequency == 0) continue;
+            if (frequency % 2 == 0) {
+                totalLength += 2;
+            } else {
+                totalLength += 1;
+            }
+        }
+        return totalLength;
+    }
+}
 
 -----------------------------------------------------------------------------------------------------------
  
