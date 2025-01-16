37354399795d1b925dbb4efff6c831859c015b99
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2025-01-16 06:52:16 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -5260,3 +5260,17 @@ class Solution {
         return res;
     }
 }
+
+
+
+//2425.
+class Solution {
+        public int xorAllNums(int[] A, int[] B) {
+        int x = 0, y = 0;
+        for (int a: A)
+            x ^= a;
+        for (int b: B)
+            y ^= b;
+        return (A.length % 2 * y) ^ (B.length % 2 * x);
+    }
+}
