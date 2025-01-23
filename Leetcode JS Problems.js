9adda11f50c72bee45768257b78f7fa3747a2f4a
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2025-01-23 22:12:29 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -5435,3 +5435,38 @@ class Solution {
         return matrix;
     }
 }
+
+
+//1267.
+class Solution {
+    public int countServers(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int[] rowCnt=new int[m]; 
+        int[] colCnt=new int[n];
+
+        // Count servers in each row and column
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1){                   
+                    rowCnt[i]++;
+                    colCnt[j]++;                    
+                }
+            }
+        }
+
+        int cnt=0;
+        // Count communicating servers
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1){                   
+                    if(rowCnt[i]>1 || colCnt[j]>1){
+                        cnt++;
+                    }                 
+                }
+            }
+        }
+
+        return cnt;
+    }
+}
