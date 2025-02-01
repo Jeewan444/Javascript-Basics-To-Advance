87365db7dc4e5f39bb47b6970f2a6feeb3d701bd
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2025-02-01 23:49:23 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -5787,3 +5787,15 @@ public class Solution {
         return count;
     }
 }
+
+
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] % 2 == nums[i + 1] % 2) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
