dd232024e6934f901593f510b6f11e9d6bf3f4ce
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2025-02-04 09:05:05 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -5824,3 +5824,15 @@ class Solution {
         return maxLen;
     }
 }  
+
+
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int curr = nums[0], ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            curr = nums[i] > nums[i - 1] ? curr + nums[i] : nums[i];
+            ans = Math.max(ans, curr);
+        }
+        return ans;
+    }
+}
