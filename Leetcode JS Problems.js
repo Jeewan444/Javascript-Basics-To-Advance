52091e2f0c40f62861dd7771c76fcded75cc9878
Jeewan Kumar Thakur <117151238+Jeewan444@users.noreply.github.com>
2025-02-11 14:31:35 +0530

    Update Leetcode JS Problems.js

--- Leetcode JS Problems.js
+++ Leetcode JS Problems.js
@@ -5921,3 +5921,32 @@ class Solution {
         return 1L * len * (len - 1) / 2 - count;
     }
 }
+
+
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        char[] input = s.toCharArray();
+        char[] target = part.toCharArray();
+        char[] resultStack = new char[input.length];
+        int stackSize = 0, targetLength = target.length;
+        char targetEndChar = target[targetLength - 1];
+
+        for (char currentChar : input) {
+            resultStack[stackSize++] = currentChar;
+
+            if (currentChar == targetEndChar && stackSize >= targetLength) {
+                int i = stackSize - 1, j = targetLength - 1;
+                
+                while (j >= 0 && resultStack[i] == target[j]) {
+                    i--;
+                    j--;
+                }
+
+                if (j < 0) {
+                    stackSize = i + 1;
+                }
+            }
+        }
+        return new String(resultStack, 0, stackSize);
+    }
+}
